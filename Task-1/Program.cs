943ff4dfd61f8378a8fa96da4d86d1ffa0878d05
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-03 17:51:10 +0400

    Task-1

---
+++ Task-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 63, 89, 190, 56, 25, 61, 7, 3, 2910 };
+            int min = arr[0], indexOfMin = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i]) 
+                { 
+                    min = arr[i];
+                    indexOfMin = i;
+                }
+            }
+            
+            Console.WriteLine($"Minimum value of the array is {min} on the index {indexOfMin}");
+        }
+    }
+}
